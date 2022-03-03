6b7bcd0e46d021eb93de37acc0eb0ead9561008b
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2022-03-03 11:25:54 +0600

    Update PriceController.java

--- src/main/java/NaTV/Main/controllers/PriceController.java
+++ src/main/java/NaTV/Main/controllers/PriceController.java
@@ -1,14 +1,19 @@
 package NaTV.Main.controllers;
 
+import NaTV.Main.exceptions.ChannelNotFound;
 import NaTV.Main.models.dto.PriceDto;
 import NaTV.Main.models.entity.Channel;
 import NaTV.Main.models.entity.Price;
 import NaTV.Main.services.PriceService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/price")
 public class PriceController {
@@ -16,11 +21,23 @@ public class PriceController {
     private PriceService priceService;
 
     @PostMapping("/save-price")
-    public PriceDto savePrice(@RequestBody Price price){
-        return priceService.savePrice(price);
+    public ResponseEntity<?> save(@RequestBody Price price){
+        try {
+            log.info("Saving channel");
+            return ResponseEntity.status(HttpStatus.CREATED).body(priceService.savePrice(price));
+        } catch (ChannelNotFound exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
     }
     @GetMapping("/all-prices")
     public List<Price> priceChannels(){
         return priceService.priceChannels();
     }
 }
+
