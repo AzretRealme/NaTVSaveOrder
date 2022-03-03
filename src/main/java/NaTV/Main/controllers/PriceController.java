1cc908ae059533dd7013f7e3353b1779fad5b2ff
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2022-03-03 14:23:17 +0600

    Update PriceController.java

--- src/main/java/NaTV/Main/controllers/PriceController.java
+++ src/main/java/NaTV/Main/controllers/PriceController.java
@@ -1,13 +1,13 @@
 package NaTV.Main.controllers;
 
-import NaTV.Main.models.dto.PriceDto;
+import NaTV.Main.exceptions.PriceNotFound;
 import NaTV.Main.models.entity.Price;
 import NaTV.Main.services.PriceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,15 +17,37 @@ public class PriceController {
     private PriceService priceService;
 
     @PostMapping("/save-price")
-    public PriceDto save(@RequestBody Price price){
-            return priceService.savePrice(price);
+    public ResponseEntity<?> save(@RequestBody Price price){
+        try {
+            log.info("Saving price");
+            return ResponseEntity.status(HttpStatus.CREATED).body(priceService.savePrice(price));
+        } catch (PriceNotFound exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
 
     }
     @GetMapping("/all-prices")
-    public List<Price> priceChannels(){
-
-        return priceService.priceChannels();
+    public ResponseEntity<?> priceChannels(){
+        try {
+            log.info("Get-All-Price");
+            return ResponseEntity.status(HttpStatus.CREATED).body(priceService.priceChannels());
+        } catch (PriceNotFound exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
     }
 }
+
 
 
