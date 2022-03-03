b8b49d6f751e4138b5fad060fc2e663664fa40cf
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2022-03-03 12:01:48 +0600

    Update DiscountController.java

--- src/main/java/NaTV/Main/controllers/DiscountController.java
+++ src/main/java/NaTV/Main/controllers/DiscountController.java
@@ -1,14 +1,10 @@
 package NaTV.Main.controllers;
 
-import NaTV.Main.exceptions.ChannelNotFound;
 import NaTV.Main.models.dto.DiscountDto;
-import NaTV.Main.models.entity.Channel;
 import NaTV.Main.models.entity.Discount;
 import NaTV.Main.services.DiscountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,19 +18,9 @@ public class DiscountController {
     private DiscountService discountService;
 
     @PostMapping("/save-discount")
-    public ResponseEntity<?> save(@RequestBody Discount discount){
-        try {
-            log.info("Saving channel");
-            return ResponseEntity.status(HttpStatus.CREATED).body(discountService.saveDiscount(discount));
-        } catch (ChannelNotFound exception) {
-            log.error(exception.getMessage());
-            exception.printStackTrace();
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-        } catch (Exception exception) {
-            log.error(exception.getMessage());
-            exception.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
-        }
+    public DiscountDto save(@RequestBody Discount discount){
+            return discountService.saveDiscount(discount);
+
     }
     @GetMapping("/all-discounts")
     public List<Discount> discountChannels(){
@@ -43,4 +29,5 @@ public class DiscountController {
 
 
 }
+
 
