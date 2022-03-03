fb3209cf814972e2dfccecd708ec69bc1312c09a
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2022-03-03 11:26:47 +0600

    Update OrderController.java

--- src/main/java/NaTV/Main/controllers/OrderController.java
+++ src/main/java/NaTV/Main/controllers/OrderController.java
@@ -1,15 +1,20 @@
 package NaTV.Main.controllers;
 
 import NaTV.Main.dao.OrderRepo;
+import NaTV.Main.exceptions.OrderNotFound;
 import NaTV.Main.models.entity.Order;
 import NaTV.Main.models.objects.objectOrders.FinalResponse;
 import NaTV.Main.models.objects.objectOrders.OrderData;
 import NaTV.Main.services.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
@@ -18,15 +23,25 @@ public class OrderController {
     @Autowired
     private OrderRepo orderRepo;
 
-
     @GetMapping("get-Order")
-    public List<Order> getOrder(){
+    public List<Order> getOrder() {
         return orderRepo.findAll();
     }
 
     @PostMapping("save-OrderData")
-    public FinalResponse save(@RequestBody OrderData orderData){
-        return orderService.saveOrder(orderData);
+    public ResponseEntity<?> save(@RequestBody OrderData orderData) {
+        try {
+            log.info("Saving order");
+            return ResponseEntity.status(HttpStatus.CREATED).body(orderService.saveOrder(orderData));
+        } catch (OrderNotFound exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
     }
-
 }
+
